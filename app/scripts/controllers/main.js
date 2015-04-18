90380f0b88f162255c12e27e2c68be7e17bf0beb
thuslako <lukundu.lako@gmail.com>
2015-04-18 09:57:02 -0400

    updated styling and functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,18 @@
 angular.module('WeApp')
 	.controller('mainCTRL',function($scope,$http){
-	$scope.city = 'Ottawa';
 
 	$scope.getWea = function (){
-		if($scope.city){
 			$http.get('http://api.openweathermap.org/data/2.5/weather?q='+$scope.city)
 				.success(function(data, status, headers, config) {
-					console.log('get emm!');
 					$scope.wea = data;
 					if($scope.wea.main.temp)
 						$scope.temp = $scope.wea.main.temp - 273.15;
 					else if($scope.temp == -273.15)
 						$scope.temp = null; 
-					console.log(data);
+					console.log('get emm!');
 	 	    	})
 				.error(function(data, status, headers, config) {
 	  				console.log('these nuts!');	
 				});
-		}
-		$scope.city = null;
 	}
 });
